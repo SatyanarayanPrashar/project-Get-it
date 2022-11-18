30f76ba64ad896abde9d3e1805ecaed6b9898541
Mr-MeowMeow <satyanarayanprashar73@gmail.com>
2022-11-18 22:14:16 +0530

    helper set aand fetch done

--- lib/models/comment.dart
+++ lib/models/comment.dart
@@ -1,23 +1,27 @@
 class CommentModel {
   String? commentBy;
+  String? helperId;
   DateTime? commentedOn;
   String? note;
 
   CommentModel({
     this.commentBy,
     this.commentedOn,
+    this.helperId,
     this.note,
   });
 
   CommentModel.fromMap(Map<String, dynamic> map) {
     commentBy = map["commentby"];
     commentedOn = map["commentedOn"];
+    helperId = map["helperId"];
     note = map["note"];
   }
 
   Map<String, dynamic> toMap() {
     return {
       "commentby": commentBy,
+      "helperId": helperId,
       "commentedOn": commentedOn,
       "note": note,
     };
